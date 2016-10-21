8cd1cbb2a74958e26836464f5835b7d76a8e7e46
gualtyphone <gualtyphone@me.com>
2016-10-22 00:46:32 +0100

    Added RecordPosition and removed Useless stuff
    RecordPosition for falcon uses mouse button ATM
    Usless stuff is standard assets

---
+++ Assets/Scripts/RecordPreviousPos.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecordPreviousPos : MonoBehaviour {
+
+    public GameObject[] positions = new GameObject[100];
+    public int count = 0;
+    public GameObject RefObj;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButton(0))
+        {
+            if (count == 0)
+            {
+                positions[count] = Instantiate(RefObj);
+                positions[count].transform.position = transform.position;
+                count++;
+            }
+            else if (positions[count-1].transform.position != transform.position && count < 100)
+            {
+                positions[count] = Instantiate(RefObj);
+                positions[count].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                positions[count].transform.position = transform.position;
+                count++;
+            }
+            
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Destroy(positions[i]);
+            }
+            count = 0;
+        }
+	}
+}
