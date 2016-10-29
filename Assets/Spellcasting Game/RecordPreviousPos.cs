1eb42d1f7d83fae78e11bb144ca079281ebcafdf
gualtyphone <gualtyphone@me.com>
2016-10-29 07:49:20 +0100

    modified spell game

--- Assets/Spellcasting Game/RecordPreviousPos.cs
+++ Assets/Spellcasting Game/RecordPreviousPos.cs
@@ -6,16 +6,17 @@ public class RecordPreviousPos : MonoBehaviour {
     public GameObject[] positions;
     public int count = 0;
     public GameObject RefObj;
+    
 	// Use this for initialization
 	void Start () {
         positions = new GameObject[1024];
-        
     }
 	
 	// Update is called once per frame
 	void Update () {
         bool[] buttons;
         buttons = new bool[3];
+        buttons[0] = Input.GetMouseButton(0);
         //FalconUnity.getFalconButtonStates(0, out buttons);
         if (buttons[0])
         {
@@ -23,7 +24,8 @@ public class RecordPreviousPos : MonoBehaviour {
             {
                 positions[count] = Instantiate(RefObj);
                 Vector3 pos;
-                FalconUnity.getGodPosition(0, out pos);
+                //FalconUnity.getGodPosition(0, out pos);
+                pos = transform.position;
                 positions[count].transform.position = pos;
                 positions[count].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                 count++;
@@ -32,7 +34,8 @@ public class RecordPreviousPos : MonoBehaviour {
             {
                 positions[count] = Instantiate(RefObj);
                 Vector3 pos;
-                FalconUnity.getGodPosition(0, out pos);
+                pos = transform.position;
+                //FalconUnity.getGodPosition(0, out pos);
                 positions[count].transform.position = pos;
                 positions[count].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                 
@@ -44,7 +47,7 @@ public class RecordPreviousPos : MonoBehaviour {
         {
             for (int i = 0; i < count; i++)
             {
-               // Destroy(positions[i]);
+                Destroy(positions[i]);
                 //positions[i].GetComponent<FalconRigidBody>().enabled = true;
             }
             count = 0;
