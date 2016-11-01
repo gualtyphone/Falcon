23317a20d5d766e4b39c67416708bc8f1741491f
wc2-gould <William.Gould2@live.uwe.ac.uk>
2016-11-01 18:35:47 +0000

    Finished 3d maze up for testing and final fixing

---
+++ Assets/BlindGame/Scripts/winGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class winGame : MonoBehaviour {
+
+    public GameObject endMessage;
+    public float counter = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos;
+        FalconUnity.getGodPosition(0, out pos);
+        if (GetComponent<Collider>().bounds.Contains(pos))
+        {
+            Debug.Log("Win");
+            endMessage.GetComponent<Text>().enabled = true;
+            endMessage.GetComponent<Text>().text = "You Win!!!";
+        }
+	}
+
+    void OnTriggerEnter()
+    {
+       Debug.Log("Win");
+       endMessage.GetComponent<Text>().enabled = true;
+        endMessage.GetComponent<Text>().text = "You Win!!!";
+    }
+}
---
+++ Assets/DisplayStartMessage.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayStartMessage : MonoBehaviour {
+
+    public GameObject startMessage;
+    public float counter = 3.0f;
+    bool done = true;
+
+	// Use this for initialization
+	void Start () {
+               
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter -= Time.deltaTime;
+
+        if(counter <= 0 && !done){
+            Debug.Log("dest");
+            startMessage.GetComponent<Text>().enabled = false;
+            done = false;
+        }
+	}
+}
