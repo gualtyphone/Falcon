363f86148b8b5c0eda31687b8e4e593a77426ff5
joshf67 <iownall99@live.co.uk>
2016-11-01 12:56:31 +0000

    Changed enemy colour on machine gun and added power bar + release to shoot on slingshot

--- Assets/Slingshot Game/Scripts/BallControl.cs
+++ Assets/Slingshot Game/Scripts/BallControl.cs
@@ -12,13 +12,13 @@ public class BallControl : MonoBehaviour {
 	public float forceMultFar;
 	public float forceMultClose;
 	public Vector3 closeMidFar;
-	public bool keyboardControl;
-	bool fire = true;
+	public bool fire = true;
 	bool snapped = false;
 	public Vector3 currPos;
 	LineRenderer[] elastic;
 	public SphereManipulator falconManip;
 	public Text textSpace;
+    public Slider slide;
 	public int score;
 	public GameObject camera;
 	public bool[] relativeCamera = new bool[2];
@@ -29,6 +29,14 @@ public class BallControl : MonoBehaviour {
 	public Vector3 closestPoint;
 	public float margin;
 	public float distance;
+    public float prevDistance;
+    public float slideValue;
+    public float shotMargin;
+    public float testValue;
+    public float shotWait;
+    public float shotWaitTime;
+    public float resetTime;
+    public float reset;
 
 	// Use this for initialization
 	void Start () {
@@ -38,13 +46,50 @@ public class BallControl : MonoBehaviour {
 		elastic = GameObject.FindObjectsOfType<LineRenderer> ();
 		FalconUnity.getTipPosition (0, out falPos);
 		score = 0;
-	}
+        distance = Vector3.Distance(transform.position, originPos);
+        prevDistance = distance;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		FalconUnity.getTipPosition (0, out falPos);
 		distance = Vector3.Distance(transform.position,originPos);
-		if (distance > margin && fire == true) {
+        if (reset > 0)
+        {
+            reset = resetTime;
+        }
+        if (shotWait > 0)
+        {
+            shotWait -= Time.deltaTime;
+        } else
+        {
+            if (prevDistance - distance >= shotMargin && !falconManip.button_states[3] && fire == true && distance < closeMidFar.z)
+            {
+                fire = false;
+                if (distance < closeMidFar.x)
+                {
+                    force = distance * forceMultClose;
+                }
+                else if (distance >= closeMidFar.x && distance < closeMidFar.y)
+                {
+                    force = distance * (((forceMultFar - forceMultClose) / 2) + forceMultFar);
+                }
+                else
+                {
+                    force = distance * forceMultFar;
+                }
+                Rigidbody rigid = gameObject.AddComponent<Rigidbody>();
+                rigid.collisionDetectionMode = CollisionDetectionMode.Continuous;
+                rigid.AddForce(transform.forward * force);
+            }
+            prevDistance = distance;
+            shotWait = shotWaitTime;
+        }
+      
+
+        slideValue = distance / closeMidFar.z;
+        slide.value = slideValue;
+        if (distance > margin && fire == true) {
 			posDis = closestPoint - falPos;
 			FalconUnity.applyForce (0, (posDis) * distance * 90, 0.1F);
 		}
@@ -63,18 +108,17 @@ public class BallControl : MonoBehaviour {
 			textSpace.text = "click '+' to reload";
 		}
 
-		if (falconManip.button_states[3] == true) {
+		if (falconManip.button_states[3] == true && reset <= 0) {
 			parent.transform.position = originPos;
 			transform.position = parent.transform.position + ballOriginOffset;
 			foreach (LineRenderer i in elastic) {
 				i.enabled = true;
 			}
-			float time = 0;
 			fire = true;
 			snapped = false;
 			Destroy(gameObject.GetComponent<Rigidbody>());
 		}
-
+  
 		if (Input.GetKeyDown (KeyCode.R)) {
 			Application.LoadLevel(Application.loadedLevel);
 		}
@@ -96,20 +140,6 @@ public class BallControl : MonoBehaviour {
 				camera.transform.position = transform.position + flightCamera[0];
 				camera.transform.rotation = Quaternion.Euler(transform.rotation.x + flightCamera[1].x,transform.rotation.y + flightCamera[1].y,transform.rotation.z + flightCamera[1].z);
 			}
-		}
-
-		if (falconManip.button_states[1] && !falconManip.button_states[3] && fire == true && distance < closeMidFar.z) {
-			fire = false;
-			if (distance < closeMidFar.x) {
-				force = distance * forceMultClose;
-			} else if (distance >= closeMidFar.x && distance < closeMidFar.y) {
-				force = distance * (((forceMultFar - forceMultClose)/2) + forceMultFar);
-			} else {
-				force = distance * forceMultFar;
-			}
-			Rigidbody rigid = gameObject.AddComponent<Rigidbody>();
-			rigid.collisionDetectionMode = CollisionDetectionMode.Continuous;
-			rigid.AddForce(transform.forward * force);
 		}
 	}
 }
